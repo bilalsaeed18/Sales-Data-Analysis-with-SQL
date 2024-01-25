6380540d6041f7786ab37ec1cfafd3bf43ea81e3
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-01-25 14:55:47 +0500

    Adding files

--- Sales_DataAnalysis.sql
+++ Sales_DataAnalysis.sql
@@ -3,3 +3,11 @@
 
 -- Select all data form the table
 select * from Sales
+
+-- Total Revenue
+select sum(avg_price), Transaction_ID from Sales
+group by Transaction_ID
+
+
+select * from Sales
+where CustomerID = '13047' and Transaction_ID = '16687'
