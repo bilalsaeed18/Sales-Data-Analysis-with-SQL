d69293f696fd3d786321eff403688a2186a792e4
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-01-26 16:08:28 +0500

    Update SQL

--- Sales_DataAnalysis.sql
+++ Sales_DataAnalysis.sql
@@ -10,7 +10,7 @@ select COUNT(Distinct ORDERNUMBER) from sales_data
 
 -- Total sales by country
 
-select country, sum(round (sales,2)), as Sales from sales_data
+select country, sum(round (sales,2)) as Sales from sales_data
 group by country
 order by 2 desc
 
@@ -19,3 +19,16 @@ order by 2 desc
 select  PRODUCTCODE, sum(QUANTITYORDERED) AS 'Product Qty Ordered', sum(round (sales,2)) as Sales from sales_data
 group by PRODUCTCODE
 order by 2 desc
+
+-- Total sales by Year 
+
+select  YEAR_ID as 'Year', sum(round (sales,2)) as Sales 
+from sales_data
+group by YEAR_ID
+
+-- AVG QTY Sold and Sales per Product
+
+select  Distinct ORDERNUMBER, sum(round (sales,2)) as Sales, AVG(QUANTITYORDERED) as 'AVG QTY Sold' from sales_data
+group by ORDERNUMBER
+order by 2 desc
+
